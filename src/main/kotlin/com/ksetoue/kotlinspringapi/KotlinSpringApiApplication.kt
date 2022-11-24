df532e5b459187979dbb7925d7b38fd8d53f5d79
ksetoue <karoline.kimiko@gmail.com>
2022-11-23 22:25:36 -0300

    :tada: initialize project

---
+++ src/main/kotlin/com/ksetoue/kotlinspringapi/KotlinSpringApiApplication.kt
@@ -0,0 +1,11 @@
+package com.ksetoue.kotlinspringapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/ksetoue/kotlinspringapi/KotlinSpringApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ksetoue.kotlinspringapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
