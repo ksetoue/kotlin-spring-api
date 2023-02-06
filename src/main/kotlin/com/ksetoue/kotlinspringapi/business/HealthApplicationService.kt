2f3b571d221578a15a4e728d894df1d49f45323b
ksetoue <karoline.kimiko@gmail.com>
2023-02-06 02:14:16 -0300

    :recycle: rename packages

--- src/main/kotlin/com/ksetoue/kotlinspringapi/business/HealthApplicationService.kt
+++ src/main/kotlin/com/ksetoue/kotlinspringapi/business/HealthApplicationService.kt
@@ -1,14 +1,21 @@
 package com.ksetoue.kotlinspringapi.application
 
 import com.ksetoue.kotlinspringapi.domain.HealthContext
+import io.opentracing.Span
+import io.opentracing.util.GlobalTracer
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class HealthApplicationService {
     private val logger = LoggerFactory.getLogger(this::class.java)
+    private val tracer = GlobalTracer.get()
+
+    private val span: Span = tracer.buildSpan("HealthApplicationService").start()
+    private val scope = tracer.scopeManager().activate(span)
+
     fun getStatus(): HealthContext {
-        logger.info("getStatus")
+        span.log("get-status")
         return HealthContext("running")
     }
 }
